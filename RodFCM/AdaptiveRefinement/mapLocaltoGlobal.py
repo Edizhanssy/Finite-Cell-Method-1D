2fb3390ed368a62aaa0309333a64b3dfb42fc41c
Edizhan Yildiz <128889535+Edizhanssy@users.noreply.github.com>
2024-02-08 21:36:48 +0300

    Delete RodFCM/AdaptiveRefinement/mapLocaltoGlobal.py

--- RodFCM/AdaptiveRefinement/mapLocaltoGlobal.py
+++
@@ -1,6 +0,0 @@
-
-def map_local_to_global(local_coords, element_global_coords):
-    global_start = element_global_coords[0] + local_coords[0] * (element_global_coords[1] - element_global_coords[0])
-    global_end = element_global_coords[0] + local_coords[1] * (element_global_coords[1] - element_global_coords[0])
-
-    return (global_start, global_end)
