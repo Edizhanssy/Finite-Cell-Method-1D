2471481c4799e8fe54b0a7cae0a53bc6f9bae7a3
Edizhan Yildiz <128889535+Edizhanssy@users.noreply.github.com>
2024-02-10 22:25:57 +0300

    Delete BoundaryCondition directory

--- BoundaryCondition/NodeStrongBoundaryCondition/ApplyBoundaryConditions.py
+++
@@ -1,9 +0,0 @@
-
-def ApplyBoundaryConditions(mesh, StiffnessMatrix, ForceVector, BoundaryConditions):
-    # the number of boundary conditions are taken for appending them both stiffness matrix and force vector !
-    lengthBC = len(BoundaryConditions)
-    for MeshId in range(len(mesh)-1):
-        for i in range(lengthBC):
-            StiffnessMatrix, ForceVector = BoundaryConditions[i].appendBcs(mesh[i], StiffnessMatrix, ForceVector)
-    return StiffnessMatrix, ForceVector
-
