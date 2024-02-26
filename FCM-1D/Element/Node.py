dd0e77bd195f1c293c375aa1b8558ac754d731b7
Edizhan Yildiz <128889535+Edizhanssy@users.noreply.github.com>
2024-02-26 20:12:38 +0300

    Delete FCM-1D directory

--- FCM-1D/Element/Node.py
+++
@@ -1,9 +0,0 @@
-from Element.Dof import Dof
-# the Node object !
-class Node:
-    def __init__(self, coords, dof_dimension):
-        self.coords = coords
-        self.dof_dimension = dof_dimension
-        self.dofs = [Dof() for _ in range(dof_dimension)]
-    def get_dof(self):
-        return self.dofs
--- FCM-1D/Integration/IntegrationSubdomain/calcDeterminantJacobian.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def calcDeterminantJacobian(subdomain, localCoords):
-    detJ = 0.5 * np.linalg.norm(localCoords[0]-localCoords[1])
-    return detJ
-
-
