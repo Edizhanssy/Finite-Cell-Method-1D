Fix Legendre derivative recurrence; iterative O(p) evaluation; implement sine body load; correct strain post-processing and validation

FCM1D/1DFCM.py
@@ -10,6 +10,7 @@ from Element.ForceVector.assembleGlobalForceVector import assemble_Force_Vector
 from BoundaryCondition.NodeStrongBoundaryCondition.ApplyBoundaryConditions import ApplyBoundaryConditions
 from BoundaryCondition.PenaltyValueAlgorithm.StrongPenaltyAlgorithm import StrongPenaltyAlgorithm
 from BoundaryCondition.NodeStrongBoundaryCondition.StrongNodeDirichlet import StrongNodeDirichletBoundaryCondition
+from Element.ShapeFunctions.calcDerivativeofShapeFunction import evalShapeFunct
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -76,11 +77,13 @@ print('the integration points for each sub-domain will be also obtained from gau
 print('the Global Stiffness Matrix will be calculated !!')
 StiffnessMatrix = assemble_global_stiffness_matrix(elements, NumberOfGaussPoints, DOF, LtoG)
 
-kappa = np.linalg.cond(StiffnessMatrix)
-print('the ill-conditioning of the original stiffness matrix: ', kappa)
+#kappa = np.linalg.cond(StiffnessMatrix)
+#print('the ill-conditioning of the original stiffness matrix: ', kappa)
 
 print('the Global Force Vector will be calculated !!')
 ForceVector = assemble_Force_Vector(elements, NumberOfGaussPoints, DOF, LtoG)
+
+print(f'nodal DOF force sum = {ForceVector[[0,1,2],0].sum():.3e}   expected ~0')
 
 print('the boundary conditions are obtained')
 print('the strong penalty algorithm and strong dirichlet boundary conditions will be used')
@@ -93,46 +96,66 @@ DisplacementEnd = StrongNodeDirichletBoundaryCondition([3, 0, 0], -1, 1, Penalty
 print('appending boundary conditions to the Force vector')
 StiffnessMatrix, ForceVector = ApplyBoundaryConditions([nodes[0][0], nodes[0][2]], StiffnessMatrix, ForceVector, [InitialFix, DisplacementEnd])
 
+kappa = np.linalg.cond(StiffnessMatrix)
+print('the ill-conditioning of the original stiffness matrix: ', kappa)
+
 print('SOLUTION !!')
 solution = np.linalg.solve(StiffnessMatrix, ForceVector)
 solution = np.array(solution).flatten()
 
 
-#mask = (x_coords >= 1.0) & (x_coords <= 7/3)
-#print("mean strain in fictitious domain:", np.trapezoid(strains[mask], x_coords[mask]) / (7/3 - 1.0))
-#print("expected:", -1.0 / (4/3))
-
-
 print('POST-PROCESS')
 print('Calculating Axial Strains')
 
-Strains = []
-position = []
+samples = 400
+strains = []
+positions = []
 
-for i, element in enumerate(elements):
-    # Get local displacements for the current element
-    local_indices = LtoG[i]
-    local_displacements = solution[local_indices]
-    # Initialize array to hold strains at each Gauss point for this element
-    strains_at_gauss_points = []
-    GP = GaussQuadratureCoordinates(NumberOfGaussPoints)
-    # Calculate strain at each Gauss point
-    for j, gp in enumerate(GP):
-        # Derivative of shape functions at this Gauss point
-        dN = evalDerivOfShapeFunct(PolynomialDegree, gp)
-        strain_at_gauss_point = np.dot(dN, np.array(local_displacements).reshape(-1, 1))
-        strains_at_gauss_points.append(strain_at_gauss_point)
-    Strains.append(strains_at_gauss_points)
+for e, element in enumerate(elements):
+    x1, x2 = element.global_coordinates
+    J = 0.5 * (x2 - x1)
+    ue = solution[np.array(LtoG[e]).flatten()]
+    xi = np.linspace(-1.0, 1.0, samples)
+    pos_e = x1 + (xi + 1.0) * J
+    eps_e = np.array([float(np.dot(evalDerivOfShapeFunct(PolynomialDegree, x), ue)) / J
+                      for x in xi])
+    plt.plot(pos_e, eps_e, lw=1.2, color='tab:blue')
+    positions.append(pos_e)
+    strains.append(eps_e)
 
-global_positions = np.linspace(0, 3, 32)
-strains = np.squeeze([strain for strain in Strains]).flatten()
+positions = np.concatenate(positions)
+strains = np.concatenate(strains)
 
-plt.plot(global_positions, strains, marker='o', label='Axial Strain Distribution')
+m = (positions >= 1.0) & (positions <= 7/3)
+mean_eps = np.trapezoid(strains[m], positions[m]) / (7/3 - 1.0)
+print(f'mean strain (from strain field) = {mean_eps:8.5f}   expected -0.75')
+
+plt.axhline(-0.75, ls='--', lw=0.8, color='gray')
+plt.axvspan(1.0, 7/3, alpha=0.12, color='gray')
 plt.xlabel('Global Position')
 plt.ylabel('Axial strain')
 plt.title('Axial Strain Distribution along the uni-axial rod')
 plt.grid(True)
 plt.show()
+
+print('element 1 right end:', strains[samples-1], '| element 2 left end:', strains[samples])
+
+def u_at(x_global):
+    for e, element in enumerate(elements):
+        x1, x2 = element.global_coordinates
+        if x1 - 1e-12 <= x_global <= x2 + 1e-12:
+            xi = 2 * (x_global - x1) / (x2 - x1) - 1
+            N = evalShapeFunct(PolynomialDegree, xi)
+            return float(np.dot(N, solution[np.array(LtoG[e]).flatten()]))
+    raise ValueError(f'{x_global} is not in the domain')
+
+u0, u1, u2, u3 = u_at(0.0), u_at(1.0), u_at(7/3), u_at(3.0)
+print(f'u(0)   = {u0:12.6e}   expected  0')
+print(f'u(1)   = {u1:12.6e}   expected  0')
+print(f'u(7/3) = {u2:12.6e}   expected -1')
+print(f'u(3)   = {u3:12.6e}   expected -1')
+print(f'mean strain in fictitious = {(u2 - u1) / (7/3 - 1):8.5f}   expected -0.75')
+
 
 
 